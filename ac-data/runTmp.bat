3b6593d13126a5ac1e34ce6796e8de03baa7a4a5
Newms34 <newms3450@gmail.com>
2020-06-24 14:04:31 -0400

    some error fixin

---
+++ ac-data/runTmp.bat
@@ -0,0 +1,3 @@
+cd "E:\projects\samCheckArc"
+python ac-data/arc-check.py
+pause
--- ac-data/setup.py
+++ ac-data/setup.py
@@ -4,8 +4,6 @@ from shutil import copyfile
 
 filename = 'ac-data/run.bat'
 
-print ('CWD '+os.getcwd())
-
 if(not(os.path.exists('gw2.exe')) and not(os.path.exists('gw2-64.exe'))):
     print('Error: Could not find the Guild Wars 2 executable in this folder!')
     exit(15)
@@ -14,5 +12,5 @@ with fileinput.FileInput(filename, inplace=True) as file:
     for line in file:
         print(line.replace('potato', os.getcwd()), end='')
 
-copyfile('ac-data/run.bat','run.bat')
+copyfile('ac-data/runTmp.bat','run.bat')
     
